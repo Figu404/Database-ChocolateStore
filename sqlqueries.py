ab8b68b55159df8d04b72f73fbb226cbe30a50c9
Ida Stenberg <is222yw@student.lnu.se>
2022-03-15 17:49:45 +0100

    quary update

--- sqlqueries.py
+++ sqlqueries.py
@@ -16,27 +16,26 @@ def avrage_rate_chocklate(cursor, company):
 def cheapest_chocolate(cursor):
     query = """SELECT store.name, store.address, MIN(sell.price)
     FROM store JOIN sell ON store.name=sell.name
-    GROUP BY store.name, store.address ORDER BY MIN(sell.price) DESC"""
+    GROUP BY store.name, store.address ORDER BY MIN(sell.price) ASC"""
     cursor.execute(query)
     viewinfo(cursor,["Name","Adress", "Lowest price"])
 
 
-def stores_popular_chocolate(cursor,input):
-    query = """SELECT"""
+def stores_popular_chocolate(cursor,store):
+    query = f"""SELECT chocolate.company, chocolate.taste, AVG(likes.score)
+    FROM chocolate JOIN likes ON chocolate.product_number=likes.product_number
+    JOIN sell ON chocolate.product_number = sell.product_number
+    WHERE sell.name LIKE "%{store}%" 
+    GROUP BY chocolate.company, chocolate.taste ORDER BY AVG(likes.score) DESC"""
+    cursor.execute(query)
+    viewinfo(cursor, ["Company", "Taste", "Avrage score"])
 
 
 def shoppers(cursor):
     query = """SELECT * FROM shoppers"""
     cursor.execute(query)
     viewinfo(cursor, ["personal_code", "city", "pay"])
-
-
-
-    # """SELECT customer.personal_code, customer.address, visit.pay 
-    # FROM costumer JOIN visit ON customer.personal_code=visit.personal_code
-    # ORDER BY visit.pay DESC"""
     
-    viewinfo(cursor,["Personal_Code", "City", "pay"])
 
 def inexpensive_chocolate(cursor, company, taste):
     print(company, taste)
@@ -46,6 +45,8 @@ def inexpensive_chocolate(cursor, company, taste):
     WHERE chocolate.company LIKE "%{company}%" AND chocolate.taste LIKE "%{taste}%"
     GROUP BY store.name, store.address, company, taste ORDER BY MIN(sell.price)  ASC;""")
     viewinfo(cursor,["Store_name", "Adress", "Company_name", "Taste", "Lowest price"])
+
+
 def peoples_score(cursor):
     
     query = """SELECT """
